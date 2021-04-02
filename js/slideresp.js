4af18a25448bb37a360ea5378a0f5e2d07248f29
ejiny <jilili@outlook.jp>
2021-04-03 08:17:20 +0900

    20210403commit

---
+++ js/slideresp.js
@@ -0,0 +1,21 @@
+
+    
+var mySwiper = new Swiper('.swiper-container', {
+	effect: 'fade',
+	speed: 2000,
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
+});
